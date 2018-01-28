9e6800889b61b181037b6f723ae91cfb0ac9b88e
TheIcyOne <icy.headfishindustries@gmail.com>
2018-01-28 16:11:33 +0000

    nvm let's try that again
    should stop doing this on sugar highs

--- src/main/java/com/headfishindustries/undestructible/SpaceSavingUtils.java
+++ src/main/java/com/headfishindustries/undestructible/SpaceSavingUtils.java
@@ -1,8 +1,17 @@
 package com.headfishindustries.undestructible;
 
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.util.ArrayList;
+import java.util.List;
+
 import com.headfishindustries.undestructible.WorldStructure.BlockData;
 
 import net.minecraft.block.Block;
+import net.minecraft.nbt.CompressedStreamTools;
 import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.nbt.NBTTagList;
 import net.minecraft.tileentity.TileEntity;
@@ -10,17 +19,14 @@ import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 
 public class SpaceSavingUtils {
-	
-	//TODO: Save blocks to an NBT format
 	/** Takes input of 2 corner positions, returns an NBT tag holding all blocks within the area.**/
 	public static NBTTagCompound areaToNBT(BlockPos start, BlockPos end, World world){
-		//TODO: Optionally save entities
-		//TODO: Save tiles + contents
 		NBTTagCompound tag = new NBTTagCompound();
 		BlockPos pos;
 		
 		tag.setLong("START", start.toLong());
 		tag.setLong("END", end.toLong());
+		tag.setBoolean("ACTIVE", true);
 		
 		NBTTagCompound blockdata = new NBTTagCompound();
 	
@@ -28,29 +34,72 @@ public class SpaceSavingUtils {
 			for (int y = Math.min(start.getY(), end.getY()); y<=Math.max(start.getY(), end.getY()); y++){
 				for (int z = Math.min(start.getZ(), end.getZ()); z<=Math.max(start.getZ(), end.getZ()); z++){
 					pos = new BlockPos(x, y, z);
-					
 					BlockData bd = new WorldStructure.BlockData(world.getBlockState(pos), new NBTTagCompound());
-					
-					
 					if (world.getTileEntity(pos) != null){
-						//tiles.set(tiles.tagCount(), world.getTileEntity(pos).serializeNBT());
 						bd.tile = world.getTileEntity(pos).serializeNBT();
 					}
-					
 					blockdata.setTag("" + pos.toLong(), bd.toNBT());
 				}
 			}
 		}
-		
 		tag.setTag("BLOCKS", blockdata);
-//		tag.setTag("TILES", tiles);
 		
 		return tag;
 	}
 	
-
+	public static void writeToFile(Integer id, NBTTagCompound nbt, World world){
+		if (world == null || world.getSaveHandler().getWorldDirectory() == null) return;
+		File f = new File(world.getSaveHandler().getWorldDirectory(), "Undestructible/" + id + ".dat");
+		try{
+			f.getParentFile().mkdirs();
+			DataOutputStream s = new DataOutputStream(new FileOutputStream(f));
+			CompressedStreamTools.write(nbt, s);
+			s.close();
+		}catch(Exception e){
+			Undestructible.LOGGER.error("Couldn't save file because " + e.getMessage());
+		}
+		
+	}
 	
-
+	public static NBTTagCompound readFromFile(Integer id, World world){
+		if (world == null || world.getSaveHandler().getWorldDirectory() == null) return null;
+		File f = new File(world.getSaveHandler().getWorldDirectory(), "Undestructible/" + id + ".dat");
+		if (!f.exists()){Undestructible.LOGGER.error("Tried to read a nonexistent file. Halp."); return null;}
+		try{
+			DataInputStream s = new DataInputStream(new FileInputStream(f));
+			NBTTagCompound t = CompressedStreamTools.read(s);
+			s.close();
+			return t;
+		}catch(Exception e){
+			Undestructible.LOGGER.error("Couldn't read file because " + e.getMessage());
+			return null;
+		}		
+	}
+	
+	public static int firstAvailableID(World world){
+		int i = 0;
+		while (true){
+			File f = new File(world.getSaveHandler().getWorldDirectory(), "Undestructible/" + i + ".dat");
+			if(!f.exists()) break;
+			i++;			
+		}
+		return i;
+	}
+	
+	public static boolean isActive(NBTTagCompound tag){
+		return tag.getBoolean("ACTIVE");
+	}
+	
+	public static List<NBTTagCompound> allActive(World world){
+		List<NBTTagCompound> ls = new ArrayList<NBTTagCompound>();
+		for (int i = 0; i <= firstAvailableID(world); i++){
+			NBTTagCompound x = readFromFile(i, world);
+			if (x != null || !isActive(x)){
+				ls.add(x);
+			}
+		}
+		return ls;
+	}
 
 
 }
--- src/main/java/com/headfishindustries/undestructible/WorldStructure.java
+++ src/main/java/com/headfishindustries/undestructible/WorldStructure.java
@@ -11,6 +11,7 @@ import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.nbt.NBTTagList;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
 import net.minecraftforge.fml.common.registry.ForgeRegistries;
 import net.minecraftforge.fml.common.registry.GameRegistry;
 
@@ -18,8 +19,8 @@ public class WorldStructure {
 	
 	private BlockPos start;
 	private BlockPos end;
-	private Map<BlockPos, IBlockState> stateMap = new HashMap<BlockPos, IBlockState>();
-	private List<TileEntity> tileList = new ArrayList<TileEntity>();
+	private Map<BlockPos, BlockData> blockMap = new HashMap<BlockPos, BlockData>();
+	private World world;
 	
 
 	public WorldStructure(NBTTagCompound structTag){
@@ -27,10 +28,11 @@ public class WorldStructure {
 	}
 	
 	/** I'll probably use this later if I want to build things in different places. **/
-	public WorldStructure(BlockPos startPos, BlockPos endPos, NBTTagCompound structTag) {
+	public WorldStructure(BlockPos startPos, BlockPos endPos, NBTTagCompound structTag, World worldIn) {
 		this.parseNBT(structTag);
 		this.start = startPos;
 		this.end = endPos;
+		this.world = worldIn;
 	}
 	
 	private void parseNBT(NBTTagCompound struct){
@@ -42,20 +44,25 @@ public class WorldStructure {
 			for (int y = Math.min(start.getY(), end.getY()); y<=Math.max(start.getY(), end.getY()); y++){
 				for (int z = Math.min(start.getZ(), end.getZ()); z<=Math.max(start.getZ(), end.getZ()); z++){
 					pos = new BlockPos(x, y, z);
-					
+					blockMap.put(pos, BlockData.fromNBT((NBTTagCompound) struct.getTag("" + pos.toLong())));
 				}
 			}
-		}
-		
+		}		
 		//TODO: Tiles
 	//	NBTTagList tiles = struct.getTagList("TILES", 8);
 /*		for (int i = 0; i < tiles.tagCount(); i++){
 			TileEntity e = null;
 			e.deserializeNBT(tiles.getCompoundTagAt(i));
 			tileList.add(e);
-		}*/
-		
-		
+		}*/		
+	}
+	
+	private void buildPos(BlockPos pos){
+		BlockData bd = blockMap.get(pos);
+		world.setBlockState(pos, bd.block, 2);
+		if (!bd.tile.hasNoTags()){
+			world.getTileEntity(pos).readFromNBT(bd.tile);
+		}
 	}
 	
 	public static class BlockData{
